d514cdf9d5837796e13bbd01fd41cae1cc3e7b7b
moni <138020734+Moniga-Gopinathan@users.noreply.github.com>
2024-06-04 14:58:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,131 @@
+const movieSearchBox = document.getElementById('movie-search-box');
+const searchList = document.getElementById('search-list');
+const resultGrid = document.getElementById('result-grid');
+const favoritesList = document.getElementById('favorites-list');
+let favoriteMovies = JSON.parse(localStorage.getItem('favoriteMovies')) || [];
+
+// Load favorite movies from local storage on page load
+document.addEventListener('DOMContentLoaded', displayFavoriteMovies);
+
+// load movies from API
+async function loadMovies(searchTerm) {
+    const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=fc1fef96`;
+    const res = await fetch(`${URL}`);
+    const data = await res.json();
+    if (data.Response == "True") displayMovieList(data.Search);
+}
+
+function findMovies() {
+    let searchTerm = (movieSearchBox.value).trim();
+    if (searchTerm.length > 0) {
+        searchList.classList.remove('hide-search-list');
+        loadMovies(searchTerm);
+    } else {
+        searchList.classList.add('hide-search-list');
+    }
+}
+
+function displayMovieList(movies) {
+    searchList.innerHTML = "";
+    for (let idx = 0; idx < movies.length; idx++) {
+        let movieListItem = document.createElement('div');
+        movieListItem.dataset.id = movies[idx].imdbID; // setting movie id in data-id
+        movieListItem.classList.add('search-list-item');
+        if (movies[idx].Poster != "N/A")
+            moviePoster = movies[idx].Poster;
+        else
+            moviePoster = "image_not_found.png";
+
+        movieListItem.innerHTML = `
+        <div class="search-item-thumbnail">
+            <img src="${moviePoster}">
+        </div>
+        <div class="search-item-info">
+            <h3>${movies[idx].Title}</h3>
+            <p>${movies[idx].Year}</p>
+        </div>
+        `;
+        searchList.appendChild(movieListItem);
+    }
+    loadMovieDetails();
+}
+
+function loadMovieDetails() {
+    const searchListMovies = searchList.querySelectorAll('.search-list-item');
+    searchListMovies.forEach(movie => {
+        movie.addEventListener('click', async () => {
+            searchList.classList.add('hide-search-list');
+            movieSearchBox.value = "";
+            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=fc1fef96`);
+            const movieDetails = await result.json();
+            displayMovieDetails(movieDetails);
+        });
+    });
+}
+
+function displayMovieDetails(details) {
+    resultGrid.innerHTML = `
+    <div class="movie-poster">
+        <img src="${(details.Poster != "N/A") ? details.Poster : "image_not_found.png"}" alt="movie poster">
+    </div>
+    <div class="movie-info">
+        <h3 class="movie-title">${details.Title}</h3>
+        <ul class="movie-misc-info">
+            <li class="year">Year: ${details.Year}</li>
+            <li class="rated">Ratings: ${details.Rated}</li>
+            <li class="released">Released: ${details.Released}</li>
+        </ul>
+        <p class="genre"><b>Genre:</b> ${details.Genre}</p>
+        <p class="writer"><b>Writer:</b> ${details.Writer}</p>
+        <p class="actors"><b>Actors: </b>${details.Actors}</p>
+        <p class="plot"><b>Plot:</b> ${details.Plot}</p>
+        <p class="language"><b>Language:</b> ${details.Language}</p>
+        <p class="awards"><b><i class="fas fa-award"></i></b> ${details.Awards}</p>
+        <button class="fav-button" onclick="addToFavorites('${details.imdbID}')">Add to Favorites</button>
+    </div>
+    `;
+}
+
+function addToFavorites(id) {
+    if (favoriteMovies.indexOf(id) === -1) {
+        favoriteMovies.push(id);
+        localStorage.setItem('favoriteMovies', JSON.stringify(favoriteMovies));
+        displayFavoriteMovies();
+    }
+}
+
+function removeFromFavorites(id) {
+    const index = favoriteMovies.indexOf(id);
+    if (index !== -1) {
+        favoriteMovies.splice(index, 1);
+        localStorage.setItem('favoriteMovies', JSON.stringify(favoriteMovies));
+        displayFavoriteMovies();
+    }
+}
+
+async function displayFavoriteMovies() {
+    favoritesList.innerHTML = "";
+    for (const id of favoriteMovies) {
+        const result = await fetch(`http://www.omdbapi.com/?i=${id}&apikey=fc1fef96`);
+        const details = await result.json();
+        const favoriteItem = document.createElement('div');
+        favoriteItem.classList.add('favorite-item');
+        favoriteItem.innerHTML = `
+            <img src="${(details.Poster != "N/A") ? details.Poster : "image_not_found.png"}" alt="movie poster">
+            <div>
+                <h4>${details.Title}</h4>
+                <p>${details.Year}</p>
+            </div>
+            <button class="fav-button remove" onclick="removeFromFavorites('${details.imdbID}')">Remove</button>
+        `;
+        favoritesList.appendChild(favoriteItem);
+    }
+}
+
+window.addEventListener('click', (event) => {
+    if (event.target.className != "form-control") {
+        searchList.classList.add('hide-search-list');
+    }
+});
+
+
